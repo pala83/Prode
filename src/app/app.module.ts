1f4da0f9a0e61ad881178b53490d7f33cb12d3e3
Ulises <ulipalazzoo@gmail.com>
2022-12-01 10:48:45 -0300

    estructura general del proyecto v0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './vistas/home/home.component';
+import { NavComponent } from './plantillas/nav/nav.component';
+import { FooterComponent } from './plantillas/footer/footer.component';
+import { FootballTableComponent } from './plantillas/football-table/football-table.component';
+import { GroupTableComponent } from './plantillas/group-table/group-table.component';
+import { SessionFormComponent } from './plantillas/session-form/session-form.component';
+import { PlayerFormComponent } from './plantillas/player-form/player-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    FooterComponent,
+    FootballTableComponent,
+    GroupTableComponent,
+    SessionFormComponent,
+    PlayerFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/plantillas/football-table/football-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-football-table',
+  templateUrl: './football-table.component.html',
+  styleUrls: ['./football-table.component.scss']
+})
+export class FootballTableComponent {
+
+}
