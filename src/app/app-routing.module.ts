d232aa17690e74acdd8eedc17bb6d0481f2931d2
Ulises <ulipalazzoo@gmail.com>
2022-12-01 16:05:20 -0300

    estructura vista de sesion v0.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './vistas/home/home.component';
+import { SesionComponent } from "./vistas/sesion/sesion.component";
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'sesion',
+        component: SesionComponent
+    },
+    /*
+    {
+        path: 'register',
+        component: ProdeAddPlayerComponent
+    },
+    {
+        path: 'groups',
+        component: ProdeGroupsComponent
+    },
+    {
+        path: 'login',
+        component: ProdeLoginComponent
+    }
+    */
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [HomeComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './vistas/home/home.component';
-import { NavComponent } from './plantillas/nav/nav.component';
+
+import { HeaderComponent } from './plantillas/header/header.component';
 import { FooterComponent } from './plantillas/footer/footer.component';
 import { FootballTableComponent } from './plantillas/football-table/football-table.component';
 import { GroupTableComponent } from './plantillas/group-table/group-table.component';
-import { SessionFormComponent } from './plantillas/session-form/session-form.component';
 import { PlayerFormComponent } from './plantillas/player-form/player-form.component';
+import { SesionComponent } from './vistas/sesion/sesion.component';
+import { RegisterComponent } from './plantillas/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    NavComponent,
+    HeaderComponent,
     FooterComponent,
+    routingComponents,
+    
     FootballTableComponent,
     GroupTableComponent,
-    SessionFormComponent,
-    PlayerFormComponent
+    PlayerFormComponent,
+    SesionComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
